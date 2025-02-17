40787f9581380756c6ef92d6f4e1a7f2c871d92e
YL2209 <59274897+YL2209@users.noreply.github.com>
2025-02-17 15:18:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,22 +18,34 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 ### 修改为R4A千兆版Breed直刷版
-## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
-## 1.修改 mt7621_xiaomi_mir3g-v2.dts
-export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(expr $shanchu2 - 1)
-export shanchu2=$(echo $shanchu2"d")
-sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
-echo "" >> youhua.txt
-echo "" >> youhua.txt
-export shanchu1=$(expr $shanchu1 - 1)
-export shanchu1=$(echo $shanchu1"r")
-sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-rm -rf youhua.txt
-## 2.修改mt7621.mk
-export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
-export imsize1=$(expr $imsize1 + 2)
-export imsize1=$(echo $imsize1"s")
-sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
+AIMFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-common.dtsi
+AIDFILE=target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
+[ -e "$AIMFILE" ] || AIMFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+
+MARK="// The file has been modified by@hugcabbage"
+if grep -q "$MARK" $AIMFILE; then
+    exit 0
+else
+    sed -i "1i $MARK" $AIMFILE
+fi
+
+SPECIFIC_LINE=$(sed -n '/&spi0/=' $AIMFILE)
+BASE_TEXT=$(sed '/&spi0/,/^};/d' $AIMFILE)
+
+echo "$BASE_TEXT" | sed -n "1,${SPECIFIC_LINE}p" > $AIMFILE
+sed -n '/&spi0/,/^};/p' $AIDFILE >> $AIMFILE
+echo "$BASE_TEXT" | sed -n "$SPECIFIC_LINE,\$p" >> $AIMFILE
+sed -i "${SPECIFIC_LINE}d" $AIMFILE
+
+EXFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit-v2.dts
+MT7621MK=target/linux/ramips/image/mt7621.mk
+
+if [ -e "$EXFILE" ]; then
+    sed -i 's/partition@180000/partition@50000/' $EXFILE
+    sed -i 's/0x180000 0xe70000/0x50000 0xfa0000/' $EXFILE
+    sed -i 's/partitions {/partitions: partitions {/' $AIMFILE
+    sed -i '/Device\/xiaomi_mi-router-4a-gigabit-v2/,/Mi Router 4A/ s/14784k/16000k/' $MT7621MK
+fi
+
+sed -i '/Device\/xiaomi_mi-router-4a-gigabit/,/Mi Router 4A/ s/14848k/16064k/' $MT7621MK
+sed -i '/Device\/xiaomi_mi-router-3g-v2/,/Mi Router 3G/ s/14848k/16064k/' $MT7621MK
